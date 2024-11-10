6591af2e31dd6a0153d4bf8befb2ebf5e123a3d4
Leonel Castro <barckstar.lml@gmail.com>
2024-11-09 21:06:39 -0600

    details

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./app.scss"
 import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
 
 const App = () => {
   return <div>
     <section id="Homepage">
       <Navbar/>
+      <Sidebar />
     </section>
     <section id="Services">Services</section>
     <section id="Protafolio">Parallax</section>
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,24 @@
-import Sidebar from '../sidebar/Sidebar'
 import './navbar.scss'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-        <div className="wrapper">
-            <motion.samp 
-              initial={{opacity:0, scale:0.5}}
-              animate={{opacity:1, scale:1.5}}
-              transition={{duration:0.5}}            
-              >Full Stack Developer</motion.samp>
-            <div className="social">
-                <a href="#"><img src='facebook.png' alt=''/></a>
-                <a href="#"><img src='instagram.png' alt=''/></a>
-                <a href="#"><img src='youtube.png' alt=''/></a>
-                <a href="#"><img src='dribbble.png' alt=''/></a>
-            </div>
-            <Sidebar/>
+      <div className="wrapper">
+        <motion.span 
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1.5 }}
+          transition={{ duration: 0.5 }}
+        >
+          Full Stack Developer
+        </motion.span>
+        <div className="social">
+          <a href="#"><img src='facebook.png' alt='facebook' /></a>
+          <a href="#"><img src='instagram.png' alt='instagram' /></a>
+          <a href="#"><img src='youtube.png' alt='youtube' /></a>
+          <a href="#"><img src='dribbble.png' alt='dribbble' /></a>
         </div>
+      </div>
     </div>
   )
 }
